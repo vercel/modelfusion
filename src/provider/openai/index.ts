34f6a34813a28603cc69148b604fd79f9c6e4143
Lars Grammel <lars.grammel@gmail.com>
2023-05-25 19:52:39 +0200

    Add text model.

--- src/provider/openai/index.ts
+++ src/provider/openai/index.ts
@@ -2,3 +2,4 @@ export * from "./api/OpenAIChatCompletion.js";
 export * from "./api/OpenAITextCompletion.js";
 export * from "./api/OpenAIError.js";
 export * from "./openAIChatModel.js";
+export * from "./openAITextModel.js";
---
+++ src/provider/openai/openAITextModel.ts
@@ -0,0 +1,39 @@
+import { GeneratorModel } from "../../text/generate/GeneratorModel.js";
+import {
+  OpenAITextCompletion,
+  OpenAITextCompletionModel,
+  generateOpenAITextCompletion,
+} from "./api/OpenAITextCompletion.js";
+
+export const openAITextModel = ({
+  baseUrl,
+  apiKey,
+  model,
+  temperature = 0,
+  maxTokens,
+}: {
+  baseUrl?: string;
+  apiKey: string;
+  model: OpenAITextCompletionModel;
+  temperature?: number;
+  maxTokens?: number;
+}): GeneratorModel<string, OpenAITextCompletion, string> => ({
+  vendor: "openai",
+  name: model,
+  generate: async (
+    input: string,
+    { abortSignal }
+  ): Promise<OpenAITextCompletion> =>
+    generateOpenAITextCompletion({
+      baseUrl,
+      abortSignal,
+      apiKey,
+      prompt: input,
+      model,
+      temperature,
+      maxTokens,
+    }),
+  extractOutput: async (rawOutput: OpenAITextCompletion): Promise<string> => {
+    return rawOutput.choices[0]!.text;
+  },
+});
