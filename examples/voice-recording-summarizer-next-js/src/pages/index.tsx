33006f249e730f390e358ba65b38f81e713d7697
Lars Grammel <lars.grammel@gmail.com>
2023-05-27 16:11:58 +0200

    Implement basic push-to-record.

--- examples/voice-recording-summarizer-next-js/src/pages/index.tsx
+++ examples/voice-recording-summarizer-next-js/src/pages/index.tsx
@@ -1,12 +1,55 @@
 import { Box, IconButton } from "@mui/material";
 import Head from "next/head";
 import MicIcon from "@mui/icons-material/Mic";
+import { useRef, useState } from "react";
 
 export default function Home() {
+  const mediaRecorderRef = useRef<MediaRecorder | null>(null);
+  const [isRecording, setIsRecording] = useState(false);
+  const [audioUrl, setAudioUrl] = useState<string | null>(null);
+  const audioChunksRef = useRef<Blob[]>([]);
+
+  const handleButtonPress = () => {
+    if (isRecording) return;
+
+    navigator.mediaDevices
+      .getUserMedia({ audio: true })
+      .then((stream) => {
+        const mediaRecorder = new MediaRecorder(stream);
+        mediaRecorderRef.current = mediaRecorder;
+
+        mediaRecorder.ondataavailable = (e) => {
+          audioChunksRef.current.push(e.data);
+        };
+
+        mediaRecorder.start();
+        setIsRecording(true);
+      })
+      .catch((error) => {
+        console.error("Error accessing microphone:", error);
+      });
+  };
+
+  const handleButtonRelease = () => {
+    if (mediaRecorderRef.current && isRecording) {
+      mediaRecorderRef.current.onstop = () => {
+        const audioBlob = new Blob(audioChunksRef.current, {
+          type: "audio/mp3",
+        });
+        const audioUrl = URL.createObjectURL(audioBlob);
+        setAudioUrl(audioUrl);
+        audioChunksRef.current = [];
+      };
+
+      mediaRecorderRef.current.stop();
+      setIsRecording(false);
+    }
+  };
+
   return (
     <>
       <Head>
-        <title>@lgrammel/ai-utils chat example</title>
+        <title>@lgrammel/ai-utils voice recording summarizer example</title>
       </Head>
       <Box
         component="main"
@@ -18,6 +61,23 @@ export default function Home() {
           overflow: "hidden",
         }}
       >
+        <Box
+          sx={{
+            position: "fixed",
+            top: 0,
+            left: 0,
+            right: 0,
+            height: "40px",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            background: "rgba(0,0,0,0.2)",
+            color: "lightgray",
+          }}
+        >
+          {isRecording ? "Recording..." : "Push to record"}
+        </Box>
+
         <Box
           sx={{
             display: "flex",
@@ -35,6 +95,12 @@ export default function Home() {
               height: "64px",
               borderRadius: "50%",
             }}
+            onTouchStart={handleButtonPress}
+            onTouchEnd={handleButtonRelease}
+            onMouseDown={handleButtonPress}
+            onMouseUp={handleButtonRelease}
+            onMouseLeave={handleButtonRelease}
+            onContextMenu={(e) => e.preventDefault()}
           >
             <MicIcon sx={{ fontSize: "36px" }} />
           </IconButton>
