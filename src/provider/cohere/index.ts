bb66852520b2cdb57f024fe1ea8d6008df8a6dbc
Lars Grammel <lars.grammel@gmail.com>
2023-05-30 17:15:57 +0200

    Add export.

--- src/provider/cohere/index.ts
+++ src/provider/cohere/index.ts
@@ -1,4 +1,5 @@
 export * from "./CohereError.js";
+export * from "./embedding/CohereEmbedding.js";
 export * from "./embedding/generateCohereEmbedding.js";
 export * from "./text/CohereTextCompletion.js";
 export * from "./text/CohereTextModel.js";
