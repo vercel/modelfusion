472ae393afd5134a98d6762808f224ea3c58e5be
Lars Grammel <lars.grammel@gmail.com>
2023-06-01 16:09:07 +0200

    Add example to source.

--- src/provider/stability/image/generateStabilityImage.ts
+++ src/provider/stability/image/generateStabilityImage.ts
@@ -23,6 +23,29 @@ export type StabilityImageResponse = z.infer<
  * Call the Stability AI API for image generation.
  *
  * @see https://api.stability.ai/docs#tag/v1generation/operation/textToImage
+ *
+ * @example
+ * const imageResponse = await generateStabilityImage({
+ *   apiKey: STABILITY_API_KEY,
+ *   engineId: "stable-diffusion-512-v2-1",
+ *   textPrompts: [
+ *     { text: "the wicked witch of the west" },
+ *     { text: "style of early 19th century painting", weight: 0.5 },
+ *   ],
+ *   cfgScale: 7,
+ *   clipGuidancePreset: "FAST_BLUE",
+ *   height: 512,
+ *   width: 512,
+ *   samples: 1,
+ *   steps: 30,
+ * });
+ *
+ * imageResponse.artifacts.forEach((image, index) => {
+ *   fs.writeFileSync(
+ *     `./stability-image-example-${index}.png`,
+ *     Buffer.from(image.base64, "base64")
+ *   );
+ * });
  */
 export async function generateStabilityImage({
   baseUrl = "https://api.stability.ai/v1",
