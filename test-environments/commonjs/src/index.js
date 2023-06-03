137d78190b68ec829686b5c8dc181c7c84a3681e
Lars Grammel <lars.grammel@gmail.com>
2023-06-03 14:58:14 +0200

    Add test environments.

---
+++ test-environments/commonjs/src/index.js
@@ -0,0 +1,21 @@
+const { OpenAITextGenerationModel } = require("ai-utils.js/model/openai");
+
+require("dotenv").config();
+
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY ?? "";
+
+(async () => {
+  const textGenerationModel = new OpenAITextGenerationModel({
+    apiKey: OPENAI_API_KEY,
+    model: "text-davinci-003",
+    settings: { temperature: 0.7 },
+  });
+
+  const response = await textGenerationModel
+    .withSettings({ maxTokens: 500 })
+    .generate("Write a short story about a robot learning to love:\n\n");
+
+  const text = await textGenerationModel.extractOutput(response);
+
+  console.log(text);
+})();
