6915d968d251bb233b99363b4092c0e0fc5705e9
Lars Grammel <lars.grammel@gmail.com>
2023-06-04 10:59:11 +0200

    Fix redirect.

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -2,5 +2,5 @@ import React from "react";
 import { Redirect } from "@docusaurus/router";
 
 export default function Home() {
-  return <Redirect to="concepts/" />;
+  return <Redirect to="concept/" />;
 }
