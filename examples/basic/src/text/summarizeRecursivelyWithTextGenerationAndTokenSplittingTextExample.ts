26b38918cdd8c1841f26e59d8957508af21ba13e
Lars Grammel <lars.grammel@gmail.com>
2023-06-05 15:32:31 +0200

    Add recursive summarization example.

---
+++ examples/basic/src/text/summarizeRecursivelyWithTextGenerationAndTokenSplittingTextExample.ts
@@ -0,0 +1,37 @@
+import {
+  OpenAIChatModel,
+  summarizeRecursivelyWithTextGenerationAndTokenSplitting,
+} from "ai-utils.js";
+import dotenv from "dotenv";
+import fs from "node:fs";
+
+dotenv.config();
+
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY ?? "";
+
+const summarize =
+  summarizeRecursivelyWithTextGenerationAndTokenSplitting.asFunction({
+    model: new OpenAIChatModel({
+      apiKey: OPENAI_API_KEY,
+      model: "gpt-3.5-turbo",
+    }),
+    prompt: async ({ text }) => {
+      return [
+        { role: "system" as const, content: "Summarize the following text:" },
+        { role: "user" as const, content: text },
+      ];
+    },
+    reservedCompletionTokens: 1000,
+  });
+
+(async () => {
+  const sanFranciscoWikipedia = JSON.parse(
+    fs.readFileSync("data/san-francisco-wikipedia.json", "utf8")
+  ).content;
+
+  const summary = await summarize({
+    text: sanFranciscoWikipedia,
+  });
+
+  console.log(summary);
+})();
