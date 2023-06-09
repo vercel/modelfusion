6fcc30cb0ca7f280f59490d020a48bcee991b6ec
Lars Grammel <lars.grammel@gmail.com>
2023-06-09 21:34:05 +0200

    Update HuggingFace example.

--- examples/basic/src/model-provider/huggingface/HuggingFaceTextGenerationModelExample.ts
+++ examples/basic/src/model-provider/huggingface/HuggingFaceTextGenerationModelExample.ts
@@ -9,14 +9,13 @@ const HUGGINGFACE_API_KEY = process.env.HUGGINGFACE_API_KEY ?? "";
   const textGenerationModel = new HuggingFaceTextGenerationModel({
     apiKey: HUGGINGFACE_API_KEY,
     model: "tiiuae/falcon-7b",
-    settings: { temperature: 700 },
+    temperature: 700,
+    maxNewTokens: 500,
   });
 
-  const response = await textGenerationModel
-    .withSettings({ maxNewTokens: 500 })
-    .generate("Write a short story about a robot learning to love:\n\n");
-
-  const text = await textGenerationModel.extractText(response);
+  const text = await textGenerationModel.generateText(
+    "Write a short story about a robot learning to love:\n\n"
+  );
 
   console.log(text);
 })();
