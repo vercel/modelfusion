fa85c66e49dff311fdb537154bb5173ba1a37a9d
Lars Grammel <lars.grammel@gmail.com>
2023-06-09 21:47:59 +0200

    Cleanup example.

--- examples/basic/src/model-provider/huggingface/HuggingFaceTextGenerationModelExample.ts
+++ examples/basic/src/model-provider/huggingface/HuggingFaceTextGenerationModelExample.ts
@@ -3,11 +3,8 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const HUGGINGFACE_API_KEY = process.env.HUGGINGFACE_API_KEY ?? "";
-
 (async () => {
   const textGenerationModel = new HuggingFaceTextGenerationModel({
-    apiKey: HUGGINGFACE_API_KEY,
     model: "tiiuae/falcon-7b",
     temperature: 700,
     maxNewTokens: 500,
