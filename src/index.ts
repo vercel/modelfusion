2f29018b41677b56b935ddd146faf5849ed956b6
Lars Grammel <lars.grammel@gmail.com>
2023-06-10 14:17:43 +0200

    Rename to model-provider.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export * from "./image/index.js";
-export * from "./model/index.js";
+export * from "./model-provider/index.js";
 export * from "./run/index.js";
 export * from "./text/index.js";
 export * from "./util/index.js";
