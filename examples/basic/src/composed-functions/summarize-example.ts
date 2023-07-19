2e621a994710fe3209d84a7066a0658de5eaaed9
Lars Grammel <lars.grammel@gmail.com>
2023-07-19 20:03:37 +0200

    Restructure basic example folders.

--- examples/basic/src/composed-functions/summarize-example.ts
+++ examples/basic/src/composed-functions/summarize-example.ts
@@ -11,7 +11,8 @@ dotenv.config();
 const summarize = ({ text }: { text: string }) =>
   summarizeRecursivelyWithTextGenerationAndTokenSplitting({
     model: new OpenAIChatModel({ model: "gpt-3.5-turbo" }),
-    prompt: (input) => [
+    text,
+    prompt: async ({ text }) => [
       OpenAIChatMessage.system("Summarize the following text:"),
       OpenAIChatMessage.user(text),
     ],
