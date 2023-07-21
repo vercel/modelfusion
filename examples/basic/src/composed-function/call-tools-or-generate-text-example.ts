d15fdc2e8b2e29d60e72f1e40f251efc32eea908
Lars Grammel <lars.grammel@gmail.com>
2023-07-21 23:34:20 +0200

    Update example.

--- examples/basic/src/composed-function/call-tools-or-generate-text-example.ts
+++ examples/basic/src/composed-function/call-tools-or-generate-text-example.ts
@@ -12,7 +12,7 @@ dotenv.config();
 
 (async () => {
   const multiplyTool = new Tool({
-    name: "multiply",
+    name: "multiply" as const,
     description: "Multiply two numbers",
 
     inputSchema: z.object({
@@ -23,11 +23,26 @@ dotenv.config();
     run: async ({ a, b }) => a * b,
   });
 
+  const addTool = new Tool({
+    name: "add" as const,
+    description: "Add two numbers",
+
+    inputSchema: z.object({
+      a: z.number().describe("The first number."),
+      b: z.number().describe("The second number."),
+    }),
+
+    run: async ({ a, b }) => a + b,
+  });
+
   const result = await callToolOrGenerateText(
     new OpenAIChatModel({ model: "gpt-3.5-turbo" }),
-    [multiplyTool],
+    [multiplyTool, addTool],
+    // TODO enable easily working with passed-in tools.
     OpenAIChatFunctionPrompt.forToolChoice([
+      OpenAIChatMessage.system("You have 2 tools available (add, multiply)."),
       OpenAIChatMessage.user("What's fourteen to the power of two?"),
+      // OpenAIChatMessage.user("What's twelwe plus 1234?"),
       // OpenAIChatMessage.user("Tell me about Berlin"),
     ])
   );
