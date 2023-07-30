27833cdb67d94aca92220d458a98d5deab5f38cd
Lars Grammel <lars.grammel@gmail.com>
2023-07-30 11:24:30 +0200

    Match Llama2 prompt closely to original.

--- src/prompt/Llama2PromptMapping.ts
+++ src/prompt/Llama2PromptMapping.ts
@@ -1,6 +1,12 @@
 import { PromptMapping } from "./PromptMapping.js";
 import { InstructionPrompt } from "./InstructionPrompt.js";
 
+// see https://github.com/facebookresearch/llama/blob/6c7fe276574e78057f917549435a2554000a876d/llama/generation.py#L44
+const BEGIN_INST = "[INST]";
+const END_INST = "[/INST]";
+const BEGIN_SYS = "<<SYS>>\n";
+const END_SYS = "\n<</SYS>>\n\n";
+
 /**
  * Maps an instruction prompt to the Llama2 prompt format.
  *
@@ -12,7 +18,9 @@ export const Llama2InstructionPromptMapping: PromptMapping<
 > = {
   stopTokens: ["</s>"],
   map: (instruction) =>
-    `<s>[INST]${
-      instruction.system != null ? ` <<SYS>>${instruction.system}<</SYS>>` : ""
-    } ${instruction.instruction} [/INST]\n`,
+    `<s>${BEGIN_INST}${
+      instruction.system != null
+        ? ` ${BEGIN_SYS}${instruction.system}${END_SYS}`
+        : ""
+    } ${instruction.instruction} ${END_INST}\n`,
 };
