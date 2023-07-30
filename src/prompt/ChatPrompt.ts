68ec9eb171e2631503a8a1bf2cae453fa3d3df5b
Lars Grammel <lars.grammel@gmail.com>
2023-07-30 15:29:28 +0200

    Add ChatToTextPromptMapping,

--- src/prompt/ChatPrompt.ts
+++ src/prompt/ChatPrompt.ts
@@ -1,38 +1,27 @@
-type SystemMessage = { system: string };
-type UserMessage = { user: string };
-type AiMessage = { ai: string };
+export type ChatPromptSystemMessage = { system: string };
+export type ChatPromptUserMessage = { user: string };
+export type ChatPromptAiMessage = { ai: string };
 
-// Inner messages should be alternating between a user message and an ai message.
-// This is hard to achieve with TypeScript. Event solutions such as
-// https://stackoverflow.com/a/69800688
-// fail when the inner messages are dynamically created (which is typical for chat systems).
+/**
+ * A chat prompt is a sequence of messages.
+ * It can optionally start with a system message.
+ * The first message should be a user message.
+ * Then it should be alternating between a user message and an ai message.
+ * It always ends with a user message.
+ *
+ * The type checking is done at runtime.
+ */
+// Statically type checking this is impossible to achieve with TypeScript.
+// Partial solutions such as https://stackoverflow.com/a/69800688
+// fail when the inner messages are dynamically created
+// (which is typical for chat systems).
 //
 // Therefore the check that the messages are alternating is done at runtime.
-type InnerMessages = (UserMessage | AiMessage)[];
-
 export type ChatPrompt =
-  | [UserMessage]
-  | [UserMessage, AiMessage, UserMessage] // force start to provide some type checking
-  | [UserMessage, AiMessage, UserMessage, AiMessage, UserMessage] // force start to provide some type checking
+  | [...(ChatPromptUserMessage | ChatPromptAiMessage)[]]
   | [
-      UserMessage,
-      AiMessage,
-      UserMessage,
-      AiMessage,
-      ...InnerMessages,
-      UserMessage,
-    ]
-  | [SystemMessage, UserMessage]
-  | [SystemMessage, UserMessage, AiMessage, UserMessage] // force start to provide some type checking
-  | [SystemMessage, UserMessage, AiMessage, UserMessage, AiMessage, UserMessage] // force start to provide some type checking
-  | [
-      SystemMessage,
-      UserMessage,
-      AiMessage,
-      UserMessage,
-      AiMessage,
-      ...InnerMessages,
-      UserMessage,
+      ChatPromptSystemMessage,
+      ...(ChatPromptUserMessage | ChatPromptAiMessage)[],
     ];
 
 export class ChatPromptValidationError extends Error {
--- src/prompt/TextPromptMapping.ts
+++ src/prompt/TextPromptMapping.ts
@@ -1,5 +1,6 @@
 import { PromptMapping } from "./PromptMapping.js";
 import { InstructionPrompt } from "./InstructionPrompt.js";
+import { ChatPrompt, validateChatPrompt } from "./ChatPrompt.js";
 
 export const InstructionToTextPromptMapping: () => PromptMapping<
   InstructionPrompt,
@@ -10,4 +11,50 @@ export const InstructionToTextPromptMapping: () => PromptMapping<
     instruction.system != null
       ? `${instruction.system}\n\n${instruction.instruction}`
       : instruction.instruction,
+});
+
+export const ChatToTextPromptMapping: ({
+  user,
+  ai,
+}: {
+  user: string;
+  ai: string;
+}) => PromptMapping<ChatPrompt, string> = ({ user, ai }) => ({
+  map: (chatPrompt) => {
+    validateChatPrompt(chatPrompt);
+
+    let text = "";
+
+    for (let i = 0; i < chatPrompt.length; i++) {
+      const message = chatPrompt[i];
+
+      // system message:
+      if (
+        i === 0 &&
+        "system" in message &&
+        typeof message.system === "string"
+      ) {
+        text += `${message.system}\n\n`;
+        continue;
+      }
+
+      // user message
+      if ("user" in message) {
+        text += `${user}: ${message.user}\n\n`;
+        continue;
+      }
+
+      // ai message:
+      if ("ai" in message) {
+        text += `${ai}: ${message.ai}\n\n`;
+        continue;
+      }
+
+      // unsupported message:
+      throw new Error(`Unsupported message: ${JSON.stringify(message)}`);
+    }
+
+    return text;
+  },
+  stopTokens: [`\n${user}:`],
 });
