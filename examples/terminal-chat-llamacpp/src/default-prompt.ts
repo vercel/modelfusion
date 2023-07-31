975a2b559569d7efa578e4c33969e945495720b6
Lars Grammel <lars.grammel@gmail.com>
2023-07-31 19:23:01 +0200

    Update example.

--- examples/terminal-chat-llamacpp/src/default-prompt.ts
+++ examples/terminal-chat-llamacpp/src/default-prompt.ts
@@ -1,5 +1,4 @@
 import {
-  ChatPrompt,
   ChatToTextPromptMapping,
   LlamaCppTextGenerationModel,
   streamText,
@@ -7,33 +6,37 @@ import {
 } from "ai-utils.js";
 import * as readline from "node:readline/promises";
 
+// Llama 2 system prompt:
+const systemPrompt = `
+You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe. Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature.
+
+If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information.
+`;
+
 const chat = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
 (async () => {
-  const messages: Array<
-    { system: string } | { user: string } | { ai: string }
-  > = [
-    {
-      system:
-        "You are an AI assistant. Follow the user's instructions carefully.",
-    },
-  ];
+  const messages: Array<{ user: string } | { ai: string }> = [];
 
   while (true) {
     const userInput = await chat.question("You: ");
 
     messages.push({ user: userInput });
 
-    const model = new LlamaCppTextGenerationModel().mapPrompt(
-      ChatToTextPromptMapping({ user: "user", ai: "assistant" })
-    );
+    const model = new LlamaCppTextGenerationModel({
+      contextWindowSize: 4096, // Llama 2 context window size, adjust for other models
+      nPredict: 512,
+    }).mapPrompt(ChatToTextPromptMapping({ user: "user", ai: "assistant" }));
 
     const responseStream = await streamText(
       model,
-      await trimChatPrompt({ prompt: messages as ChatPrompt, model })
+      await trimChatPrompt({
+        prompt: [{ system: systemPrompt }, ...messages],
+        model,
+      })
     );
 
     let fullResponse = "";
--- examples/terminal-chat-llamacpp/src/llama2-prompt.ts
+++ examples/terminal-chat-llamacpp/src/llama2-prompt.ts
@@ -2,8 +2,16 @@ import {
   ChatToLlama2PromptMapping,
   LlamaCppTextGenerationModel,
   streamText,
+  trimChatPrompt,
 } from "ai-utils.js";
 import * as readline from "node:readline/promises";
+
+// Llama 2 system prompt:
+const systemPrompt = `
+You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe. Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature.
+
+If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information.
+`;
 
 const chat = readline.createInterface({
   input: process.stdin,
@@ -18,15 +26,17 @@ const chat = readline.createInterface({
 
     messages.push({ user: userInput });
 
+    const model = new LlamaCppTextGenerationModel({
+      contextWindowSize: 4096, // Llama 2 context window size
+      nPredict: 512,
+    }).mapPrompt(ChatToLlama2PromptMapping());
+
     const responseStream = await streamText(
-      new LlamaCppTextGenerationModel().mapPrompt(ChatToLlama2PromptMapping()),
-      [
-        {
-          system:
-            "You are an AI assistant. Follow the user's instructions carefully.",
-        },
-        ...messages,
-      ]
+      model,
+      await trimChatPrompt({
+        prompt: [{ system: systemPrompt }, ...messages],
+        model,
+      })
     );
 
     let fullResponse = "";
