c2cd4591055c36ff05fd222f44dc30e4dc557b1d
Lars Grammel <lars.grammel@gmail.com>
2023-08-05 11:01:02 +0200

    Add Helicone examples.

--- examples/basic/src/observability/helicone/helicone-openai-chat-generate-text-example.ts
+++ examples/basic/src/observability/helicone/helicone-openai-chat-generate-text-example.ts
@@ -3,9 +3,19 @@ import { OpenAIChatMessage, OpenAIChatModel, generateText } from "modelfusion";
 
 dotenv.config();
 
+const HELICONE_API_KEY = process.env.HELICONE_API_KEY;
+
+if (HELICONE_API_KEY == null) {
+  throw new Error("Please set the HELICONE_API_KEY environment variable");
+}
+
 (async () => {
   const { text } = await generateText(
     new OpenAIChatModel({
+      baseUrl: "https://oai.hconeai.com/v1",
+      headers: {
+        "Helicone-Auth": `Bearer ${HELICONE_API_KEY}`,
+      },
       model: "gpt-3.5-turbo",
       temperature: 0.7,
       maxTokens: 500,
---
+++ examples/basic/src/observability/helicone/helicone-openai-text-generate-text-example.ts
@@ -0,0 +1,27 @@
+import { OpenAITextGenerationModel, generateText } from "modelfusion";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const HELICONE_API_KEY = process.env.HELICONE_API_KEY;
+
+if (HELICONE_API_KEY == null) {
+  throw new Error("Please set the HELICONE_API_KEY environment variable");
+}
+
+(async () => {
+  const { text } = await generateText(
+    new OpenAITextGenerationModel({
+      baseUrl: "https://oai.hconeai.com/v1",
+      headers: {
+        "Helicone-Auth": `Bearer ${HELICONE_API_KEY}`,
+      },
+      model: "text-davinci-003",
+      temperature: 0.7,
+      maxTokens: 500,
+    }),
+    "Write a short story about a robot learning to love:\n\n"
+  );
+
+  console.log(text);
+})();
