7bbc2ee794f6d098af46d4986c2e910df52ce09a
Lars Grammel <lars.grammel@gmail.com>
2023-08-09 19:00:44 +0200

    Combine main and runBabyAGI.

--- examples/babyagi-agent/src/BabyAGI.ts
+++ examples/babyagi-agent/src/BabyAGI.ts
@@ -1,7 +1,28 @@
-import { OpenAITextGenerationModel, generateText } from "modelfusion";
 import chalk from "chalk";
+import { Command } from "commander";
+import dotenv from "dotenv";
+import { OpenAITextGenerationModel, generateText } from "modelfusion";
 
-export async function runBabyAGI({
+dotenv.config();
+
+const program = new Command();
+
+program
+  .description("BabyAGI")
+  .requiredOption("-o, --objective <value>", "Objective")
+  .parse(process.argv);
+
+const { objective } = program.opts();
+
+runBabyAGI({
+  objective,
+  firstTask: "Develop a task list.",
+}).catch((error) => {
+  console.error(error);
+  process.exit(1);
+});
+
+async function runBabyAGI({
   objective,
   firstTask,
 }: {
