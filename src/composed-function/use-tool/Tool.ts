7a2690b42bae9aff33ed80970a96e7f39742cb52
Lars Grammel <lars.grammel@gmail.com>
2023-08-12 08:49:44 +0200

    Add optional output schema. Turn execute into RunFunction.

--- src/composed-function/use-tool/Tool.ts
+++ src/composed-function/use-tool/Tool.ts
@@ -1,21 +1,25 @@
 import { z } from "zod";
 import { SchemaDefinition } from "../../model-function/generate-json/SchemaDefinition.js";
+import { RunFunction } from "../../run/RunFunction.js";
 
 export class Tool<NAME extends string, INPUT, OUTPUT> {
   readonly name: NAME;
   readonly description: string;
   readonly inputSchema: z.ZodSchema<INPUT>;
-  readonly execute: (input: INPUT) => PromiseLike<OUTPUT>;
+  readonly outputSchema?: z.ZodSchema<OUTPUT>;
+  readonly execute: RunFunction<INPUT, OUTPUT>;
 
   constructor(options: {
     name: NAME;
     description: string;
     inputSchema: z.ZodSchema<INPUT>;
+    outputSchema?: z.ZodSchema<OUTPUT>;
     execute(input: INPUT): Promise<OUTPUT>;
   }) {
     this.name = options.name;
     this.description = options.description;
     this.inputSchema = options.inputSchema;
+    this.outputSchema = options.outputSchema;
     this.execute = options.execute;
   }
 
