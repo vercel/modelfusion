ae7e7e338ed7e264c66540b753e408ef81d873e5
Lars Grammel <lars.grammel@gmail.com>
2023-08-15 14:33:27 +0200

    Extract calculator tool.

--- examples/basic/src/tool/execute-tool-example.ts
+++ examples/basic/src/tool/execute-tool-example.ts
@@ -1,36 +1,10 @@
 import dotenv from "dotenv";
-import { Tool, executeTool } from "modelfusion";
-import { z } from "zod";
+import { executeTool } from "modelfusion";
+import { calculator } from "./calculator-tool";
 
 dotenv.config();
 
 (async () => {
-  const calculator = new Tool({
-    name: "calculator",
-    description: "Execute a calculation",
-
-    inputSchema: z.object({
-      a: z.number().describe("The first number."),
-      b: z.number().describe("The second number."),
-      operator: z.enum(["+", "-", "*", "/"]).describe("The operator."),
-    }),
-
-    execute: async ({ a, b, operator }) => {
-      switch (operator) {
-        case "+":
-          return a + b;
-        case "-":
-          return a - b;
-        case "*":
-          return a * b;
-        case "/":
-          return a / b;
-        default:
-          throw new Error(`Unknown operator: ${operator}`);
-      }
-    },
-  });
-
   const result = await executeTool(calculator, {
     a: 14,
     b: 12,
--- examples/basic/src/tool/use-tool-example.ts
+++ examples/basic/src/tool/use-tool-example.ts
@@ -1,42 +1,15 @@
+import dotenv from "dotenv";
 import {
   OpenAIChatFunctionPrompt,
   OpenAIChatMessage,
   OpenAIChatModel,
-  Tool,
   useTool,
 } from "modelfusion";
-import dotenv from "dotenv";
-import { z } from "zod";
+import { calculator } from "./calculator-tool";
 
 dotenv.config();
 
 (async () => {
-  const calculator = new Tool({
-    name: "calculator",
-    description: "Execute a calculation",
-
-    inputSchema: z.object({
-      a: z.number().describe("The first number."),
-      b: z.number().describe("The second number."),
-      operator: z.enum(["+", "-", "*", "/"]).describe("The operator."),
-    }),
-
-    execute: async ({ a, b, operator }) => {
-      switch (operator) {
-        case "+":
-          return a + b;
-        case "-":
-          return a - b;
-        case "*":
-          return a * b;
-        case "/":
-          return a / b;
-        default:
-          throw new Error(`Unknown operator: ${operator}`);
-      }
-    },
-  });
-
   const { tool, parameters, result } = await useTool(
     new OpenAIChatModel({ model: "gpt-3.5-turbo" }),
     calculator,
