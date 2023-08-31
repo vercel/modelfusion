0ff53ceed1c64e9fb087c6bfb0a5281106271d8b
Lars Grammel <lars.grammel@gmail.com>
2023-08-31 14:52:01 +0200

    Split examples. Add global logging and override examples.

---
+++ examples/basic/src/util/logging/basic-text-function-logging-example.ts
@@ -0,0 +1,19 @@
+import { OpenAITextGenerationModel, generateText } from "modelfusion";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+(async () => {
+  console.log();
+  console.log("Logging: basic-text");
+  console.log();
+
+  const text = await generateText(
+    new OpenAITextGenerationModel({
+      model: "text-davinci-003",
+      maxCompletionTokens: 50,
+    }),
+    "Write a short story about a robot learning to love:\n\n",
+    { logging: "basic-text" }
+  );
+})();
---
+++ examples/basic/src/util/logging/basic-text-global-logging-example.ts
@@ -0,0 +1,24 @@
+import {
+  OpenAITextGenerationModel,
+  generateText,
+  setGlobalFunctionLogging,
+} from "modelfusion";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+(async () => {
+  console.log();
+  console.log("Logging: basic-text");
+  console.log();
+
+  setGlobalFunctionLogging("basic-text");
+
+  const text = await generateText(
+    new OpenAITextGenerationModel({
+      model: "text-davinci-003",
+      maxCompletionTokens: 50,
+    }),
+    "Write a short story about a robot learning to love:\n\n"
+  );
+})();
