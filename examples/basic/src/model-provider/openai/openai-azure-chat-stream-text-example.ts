9acf2ce13ed024029a8839d95cd2605c6813b2d3
Lars Grammel <lars.grammel@gmail.com>
2023-09-07 13:08:42 +0200

    Cleanup example.

--- examples/basic/src/model-provider/openai/openai-azure-chat-stream-text-example.ts
+++ examples/basic/src/model-provider/openai/openai-azure-chat-stream-text-example.ts
@@ -12,9 +12,8 @@ dotenv.config();
   const textStream = await streamText(
     new OpenAIChatModel({
       model: "gpt-3.5-turbo",
-      // resolves to https://{my-resource-name}.openai.azure.com/openai/deployments/{my-deployment-id}/{path}?api-version={my-api-version}
       api: new AzureOpenAIApiConfiguration({
-        // apiKey: process.env.AZURE_OPENAI_API_KEY, -- automatically loaded from environment variable
+        // apiKey: automatically uses process.env.AZURE_OPENAI_API_KEY,
         resourceName: "my-resource-name",
         deploymentId: "my-deployment-id",
         apiVersion: "my-api-version",
