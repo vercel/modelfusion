15fb6c05c153771ed3c58a343d1d51ba4b03420b
Lars Grammel <lars.grammel@gmail.com>
2023-09-09 09:48:45 +0200

    Document ApiConfiguration.

--- src/model-function/ApiConfiguration.ts
+++ src/model-function/ApiConfiguration.ts
@@ -2,16 +2,35 @@ import { RetryFunction } from "../util/api/RetryFunction.js";
 import { ThrottleFunction } from "../util/api/ThrottleFunction.js";
 
 /**
- * Settings for a REST provider, e.g. OpenAI or Lmnt.
+ * Settings for how to call an API, e.g. OpenAI.
+ *
+ * This interfaces enables creating pre-defined API configuration for certain API (e.g., OpenAI),
+ * pre-defined proxy configurations (e.g. Helicone OpenAI),
+ * and fully custom setups (e.g. your own internal OpenAI proxy with custom headers).
  */
 export interface ApiConfiguration {
   /**
    * Creates a full URL given a inner path.
+   *
+   * The inner path always starts with a slash, e.g. `/generate`.
    */
   assembleUrl(path: string): string;
 
+  /**
+   * Returns the headers that should be included in every request to the API.
+   * This is intended for authentication headers and proxy settings.
+   *
+   * The model may add additional headers, in particular "Content-Type"."
+   */
   readonly headers: Record<string, string>;
 
+  /**
+   * Defines how failed API calls should be retried.
+   */
   readonly retry?: RetryFunction;
+
+  /**
+   * Defines how API calls should be rate limited.
+   */
   readonly throttle?: ThrottleFunction;
 }
