773d312fe2aada8290da3a1048bb79cbca049c72
Lars Grammel <lars.grammel@gmail.com>
2023-09-09 10:31:41 +0200

    Add documentation to BaseUrlApiConfiguration.

--- src/core/api/BaseUrlApiConfiguration.ts
+++ src/core/api/BaseUrlApiConfiguration.ts
@@ -2,6 +2,11 @@ import { AbstractApiConfiguration } from "./AbstractApiConfiguration.js";
 import { RetryFunction } from "./RetryFunction.js";
 import { ThrottleFunction } from "./ThrottleFunction.js";
 
+/**
+ * An API configuration that uses a base URL and a set of headers.
+ *
+ * You can use it to configure custom APIs for models, e.g. your own internal OpenAI proxy with custom headers.
+ */
 export class BaseUrlApiConfiguration extends AbstractApiConfiguration {
   readonly baseUrl: string;
   readonly headers: Record<string, string>;
