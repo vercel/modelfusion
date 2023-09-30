80807e82f4eee40edadfcd25efb9b027511ea9d0
Lars Grammel <lars.grammel@gmail.com>
2023-09-30 22:45:43 +0200

    Add events to retrieve.

--- src/retriever/retrieve.ts
+++ src/retriever/retrieve.ts
@@ -1,4 +1,16 @@
+import { nanoid as createId } from "nanoid";
+import { FunctionEventSource } from "../core/FunctionEventSource.js";
+import { getGlobalFunctionLogging } from "../core/GlobalFunctionLogging.js";
+import { getGlobalFunctionObservers } from "../core/GlobalFunctionObservers.js";
+import { AbortError } from "../core/api/AbortError.js";
+import { getFunctionCallLogger } from "../core/getFunctionCallLogger.js";
 import { ModelFunctionOptions } from "../model-function/ModelFunctionOptions.js";
+import { startDurationMeasurement } from "../util/DurationMeasurement.js";
+import { runSafe } from "../util/runSafe.js";
+import {
+  RetrieveFinishedEvent,
+  RetrieveStartedEvent,
+} from "./RetrieveEvent.js";
 import { Retriever, RetrieverSettings } from "./Retriever.js";
 
 export async function retrieve<
@@ -10,7 +22,81 @@ export async function retrieve<
   query: QUERY,
   options?: ModelFunctionOptions<SETTINGS>
 ): Promise<OBJECT[]> {
-  // TODO add error handling, events, duration tracking, etc.
-  // TODO metadata handling
-  return retriever.retrieve(query, options);
+  const run = options?.run;
+
+  const eventSource = new FunctionEventSource({
+    observers: [
+      ...getFunctionCallLogger(options?.logging ?? getGlobalFunctionLogging()),
+      ...getGlobalFunctionObservers(),
+      ...(run?.functionObserver != null ? [run.functionObserver] : []),
+      ...(options?.observers ?? []),
+    ],
+    errorHandler: run?.errorHandler,
+  });
+
+  const durationMeasurement = startDurationMeasurement();
+
+  const startMetadata = {
+    functionType: "retrieve",
+
+    callId: `call-${createId()}`,
+    runId: run?.runId,
+    sessionId: run?.sessionId,
+    userId: run?.userId,
+    functionId: options?.functionId,
+
+    query,
+
+    timestamp: durationMeasurement.startDate,
+    startTimestamp: durationMeasurement.startDate,
+  };
+
+  eventSource.notify({
+    eventType: "started",
+    ...startMetadata,
+  } as RetrieveStartedEvent);
+
+  const result = await runSafe(() => retriever.retrieve(query, options));
+
+  const finishMetadata = {
+    eventType: "finished" as const,
+    ...startMetadata,
+    finishTimestamp: new Date(),
+    durationInMs: durationMeasurement.durationInMs,
+  };
+
+  if (!result.ok) {
+    if (result.isAborted) {
+      eventSource.notify({
+        ...finishMetadata,
+        eventType: "finished",
+        result: {
+          status: "abort",
+        },
+      } as RetrieveFinishedEvent);
+      throw new AbortError();
+    }
+
+    eventSource.notify({
+      ...finishMetadata,
+      eventType: "finished",
+      result: {
+        status: "error",
+        error: result.error,
+      },
+    } as RetrieveFinishedEvent);
+
+    throw result.error;
+  }
+
+  eventSource.notify({
+    ...finishMetadata,
+    eventType: "finished",
+    result: {
+      status: "success",
+      output: result.output,
+    },
+  } as RetrieveFinishedEvent);
+
+  return result.output;
 }
