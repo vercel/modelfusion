35000c1c6abcee88f35b489b493beb5eaa2ef379
Lars Grammel <lars.grammel@gmail.com>
2023-10-03 13:15:22 +0200

    Introduce TypeCheck.

--- src/core/structure/Schema.ts
+++ src/core/structure/Schema.ts
@@ -1,22 +1,11 @@
+import { TypeCheck } from "./TypeCheck";
+
 /**
  * Schema defines a structure for a JSON object.
  */
-export interface Schema<STRUCTURE> {
-  /**
-   * Validates that the structure of `value` matches the structure of this schema,
-   * and returns a typed version of value if it does.
-   */
-  validate(
-    value: unknown
-  ): { success: true; value: STRUCTURE } | { success: false; error: unknown };
-
+export interface Schema<STRUCTURE> extends TypeCheck<STRUCTURE> {
   /**
    * Returns the JSON schema for this schema. The schema has to be a valid JSON schema in object form.
    */
   getJsonSchema(): unknown;
-
-  /**
-   * Only used for type inference.
-   */
-  readonly _type: STRUCTURE;
 }
---
+++ src/core/structure/TypeCheck.ts
@@ -0,0 +1,17 @@
+/**
+ * Validates that the structure of `value` matches the structure of this schema.
+ */
+export interface TypeCheck<STRUCTURE> {
+  /**
+   * Validates that the structure of `value` matches the structure of this schema,
+   * and returns a typed version of value if it does.
+   */
+  validate(
+    value: unknown
+  ): { success: true; value: STRUCTURE } | { success: false; error: unknown };
+
+  /**
+   * Only used for type inference.
+   */
+  readonly _type: STRUCTURE;
+}
