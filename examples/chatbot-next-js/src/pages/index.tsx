ee0aa2c9cfd2da278d6f981692cc8dbf43a078d6
Lars Grammel <lars.grammel@gmail.com>
2023-11-01 20:37:07 +0100

    Bump ModelFusion version in example.

--- examples/chatbot-next-js/src/pages/index.tsx
+++ examples/chatbot-next-js/src/pages/index.tsx
@@ -2,9 +2,10 @@ import { ChatInputArea } from "@/component/ChatInputArea";
 import { ChatMessage } from "@/component/ChatMessage";
 import { ChatMessageInput } from "@/component/ChatMessageInput";
 import { Box, Button } from "@mui/material";
-import { ZodSchema, readEventSourceStream } from "modelfusion";
+import { ZodSchema } from "modelfusion";
+import { readEventSourceStream } from "modelfusion/browser";
 import Head from "next/head";
-import { useEffect, useRef, useState } from "react";e
+import { useEffect, useRef, useState } from "react";
 import { z } from "zod";
 
 export default function Home() {
