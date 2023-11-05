ce3539e95869793540d3f0bf81da7a80ede34606
Lars Grammel <lars.grammel@gmail.com>
2023-11-05 11:53:13 +0100

    Add tests for runSafe.

---
+++ src/util/runSafe.test.ts
@@ -0,0 +1,61 @@
+import { expect, test } from "vitest";
+import { runSafe } from "./runSafe.js";
+
+test("catch thrown error in sync function", async () => {
+  const error = new Error("test error");
+
+  const result = await runSafe(() => {
+    throw error;
+  });
+
+  expect(result).toEqual({
+    ok: false,
+    error,
+  });
+});
+
+test("catch thrown error in async function", async () => {
+  const error = new Error("test error");
+
+  const result = await runSafe(async () => {
+    throw error;
+  });
+
+  expect(result).toEqual({
+    ok: false,
+    error,
+  });
+});
+
+test("catch thrown string", async () => {
+  const result = await runSafe(async () => {
+    throw "test error";
+  });
+
+  expect(result).toEqual({
+    ok: false,
+    error: "test error",
+  });
+});
+
+test("catch rejected Promise", async () => {
+  const error = new Error("test error");
+
+  const result = await runSafe(async () => {
+    return Promise.reject(error);
+  });
+
+  expect(result).toEqual({
+    ok: false,
+    error,
+  });
+});
+
+test("no error", async () => {
+  const result = await runSafe(async () => "result");
+
+  expect(result).toEqual({
+    ok: true,
+    value: "result",
+  });
+});
