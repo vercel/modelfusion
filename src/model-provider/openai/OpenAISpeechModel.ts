35ac20c3f180d1e1939359cf61d1d9cae17eace8
Jamesb <experimentallearning0@gmail.com>
2023-11-08 06:42:11 +0000

    add pricing info

--- src/model-provider/openai/OpenAISpeechModel.ts
+++ src/model-provider/openai/OpenAISpeechModel.ts
@@ -13,7 +13,37 @@ import {
 import { OpenAIApiConfiguration } from "./OpenAIApiConfiguration.js";
 import { failedOpenAICallResponseHandler } from "./OpenAIError.js";
 
-export type OpenAISpeechModelType = "tts-1" | "tts-1-hd";
+/**
+ * @see https://openai.com/pricing
+ */
+export const OPENAI_SPEECH_MODELS = {
+  "tts-1": {
+    costInMillicentsPerCharacter: 1.5, // = 1500 / 1000,
+  },
+  "tts-1-hd": {
+    costInMillicentsPerCharacter: 3, // = 3000 / 1000
+  },
+};
+
+export type OpenAISpeechModelType = keyof typeof OPENAI_SPEECH_MODELS;
+
+export const calculateOpenAISpeechCostInMillicents = ({
+  model,
+  response,
+}: {
+  model: OpenAISpeechModelType;
+  response: any;
+}): number | null => {
+  if (!OPENAI_SPEECH_MODELS[model]) {
+    return null;
+  }
+
+  return (
+    response.input.length *
+    OPENAI_SPEECH_MODELS[model].costInMillicentsPerCharacter
+  );
+};
+
 export type OpenAISpeechVoice =
   | "alloy"
   | "echo"
