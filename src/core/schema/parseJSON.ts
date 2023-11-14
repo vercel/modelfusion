511e0ca4e4f2ad54d8c64ad947acc884e70a902e
Lars Grammel <lars.grammel@gmail.com>
2023-11-14 10:49:53 +0100

    Document parseJSON.

--- src/core/schema/parseJSON.ts
+++ src/core/schema/parseJSON.ts
@@ -1,7 +1,13 @@
 import SecureJSON from "secure-json-parse";
-import { Schema } from "./Schema.js";
 import { JSONParseError } from "./JSONParseError.js";
+import { Schema } from "./Schema.js";
 
+/**
+ * Parses a JSON string into an unknown object.
+ *
+ * @param text - The JSON string to parse.
+ * @returns {unknown} - The parsed JSON object.
+ */
 export function parseJSON({
   text,
   schema,
@@ -9,6 +15,14 @@ export function parseJSON({
   text: string;
   schema?: undefined;
 }): unknown;
+/**
+ * Parses a JSON string into a strongly-typed object using the provided schema.
+ *
+ * @template T - The type of the object to parse the JSON into.
+ * @param {string} text - The JSON string to parse.
+ * @param {Schema<T>} schema - The schema to use for parsing the JSON.
+ * @returns {T} - The parsed object.
+ */
 export function parseJSON<T>({
   text,
   schema,
@@ -46,6 +60,12 @@ export function parseJSON<T>({
   }
 }
 
+/**
+ * Safely parses a JSON string and returns the result as an object of type `unknown`.
+ *
+ * @param text - The JSON string to parse.
+ * @returns {object} Either an object with `success: true` and the parsed data, or an object with `success: false` and the error that occurred.
+ */
 export function safeParseJSON({
   text,
   schema,
@@ -55,6 +75,14 @@ export function safeParseJSON({
 }):
   | { success: true; data: unknown }
   | { success: false; error: JSONParseError };
+/**
+ * Safely parses a JSON string into a strongly-typed object, using a provided schema to validate the structure.
+ *
+ * @template T - The type of the object to parse the JSON into.
+ * @param {string} text - The JSON string to parse.
+ * @param {Schema<T>} schema - The schema to use for parsing the JSON.
+ * @returns An object with either a `success` flag and the parsed and typed data, or a `success` flag and an error object.
+ */
 export function safeParseJSON<T>({
   text,
   schema,
