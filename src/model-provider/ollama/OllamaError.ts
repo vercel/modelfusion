b2c4b9b4137cef512ac2fbf5096ac53a405844f7
Lars Grammel <lars.grammel@gmail.com>
2023-11-19 15:22:46 +0100

    Improved Ollama error parsing.

--- src/model-provider/ollama/OllamaError.ts
+++ src/model-provider/ollama/OllamaError.ts
@@ -1,30 +1,53 @@
+import { z } from "zod";
 import { ApiCallError } from "../../core/api/ApiCallError.js";
 import { ResponseHandler } from "../../core/api/postToApi.js";
+import { ZodSchema } from "../../core/schema/ZodSchema.js";
+import { parseJSON } from "../../core/schema/parseJSON.js";
+
+const ollamaErrorDataSchema = new ZodSchema(
+  z.object({
+    error: z.string(),
+  })
+);
+
+type OllamaErrorData = (typeof ollamaErrorDataSchema)["_type"];
 
 export class OllamaError extends ApiCallError {
-  public readonly data: string;
+  public readonly data: OllamaErrorData;
 
   constructor({
     statusCode,
     url,
     requestBodyValues,
-    message,
+    data,
+    message = data.error,
   }: {
-    message: string;
+    message?: string;
     statusCode: number;
     url: string;
     requestBodyValues: unknown;
+    data: OllamaErrorData;
   }) {
     super({ message, statusCode, requestBodyValues, url });
+
+    this.data = data;
   }
 }
 
 export const failedOllamaCallResponseHandler: ResponseHandler<
   ApiCallError
-> = async ({ response, url, requestBodyValues }) =>
-  new OllamaError({
+> = async ({ response, url, requestBodyValues }) => {
+  const responseBody = await response.text();
+
+  const parsedError = parseJSON({
+    text: responseBody,
+    schema: ollamaErrorDataSchema,
+  });
+
+  return new OllamaError({
     url,
     requestBodyValues,
     statusCode: response.status,
-    message: await response.text(),
+    data: parsedError,
   });
+};
