229816db9dc03052fc48ca22075784a71dc9e721
Lars Grammel <lars.grammel@gmail.com>
2023-11-23 13:22:45 +0100

    Move ToolCallParseError.

--- src/tool/generate-tool-call/index.ts
+++ src/tool/generate-tool-call/index.ts
@@ -1,4 +1,5 @@
 export * from "./TextGenerationToolCallModel.js";
 export * from "./ToolCallGenerationEvent.js";
 export * from "./ToolCallGenerationModel.js";
+export * from "./ToolCallParseError.js";
 export * from "./generateToolCall.js";
