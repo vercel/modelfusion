6b2b89b8a5ec67dcedc0dc4b90222aa8dfba2c59
Lars Grammel <lars.grammel@gmail.com>
2023-12-03 11:18:52 +0100

    Add language and temperature settings to Open AI transcription model.

--- src/model-provider/openai/OpenAITranscriptionModel.ts
+++ src/model-provider/openai/OpenAITranscriptionModel.ts
@@ -50,7 +50,25 @@ export const calculateOpenAITranscriptionCostInMillicents = ({
 export interface OpenAITranscriptionModelSettings
   extends TranscriptionModelSettings {
   api?: ApiConfiguration;
+
+  /**
+   * ID of the model to use. Only whisper-1 is currently available.
+   */
   model: OpenAITranscriptionModelType;
+
+  /**
+   * The language of the input audio. Supplying the input language in ISO-639-1 format will improve accuracy and latency.
+   */
+  language?: string; // ISO-639-1 code
+
+  /**
+   * The sampling temperature, between 0 and 1.
+   * Higher values like 0.8 will make the output more random,
+   * while lower values like 0.2 will make it more focused and deterministic.
+   * If set to 0, the model will use log probability to automatically
+   * increase the temperature until certain thresholds are hit.
+   */
+  temperature?: number;
 }
 
 export type OpenAITranscriptionInput = {
@@ -141,7 +159,10 @@ export class OpenAITranscriptionModel
   }
 
   get settingsForEvent(): Partial<OpenAITranscriptionModelSettings> {
-    return {};
+    return {
+      language: this.settings.language,
+      temperature: this.settings.temperature,
+    };
   }
 
   withSettings(additionalSettings: OpenAITranscriptionModelSettings) {
