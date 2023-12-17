4cc10fd9d6e957f51c708e6e45bcea481d7867bb
Lars Grammel <lars.grammel@gmail.com>
2023-12-17 19:16:01 +0100

    Extract shared OllamaTextGenerationSettings.

--- src/model-provider/ollama/index.ts
+++ src/model-provider/ollama/index.ts
@@ -1,7 +1,8 @@
 export * from "./OllamaApiConfiguration.js";
 export * from "./OllamaChatModel.js";
 export * from "./OllamaChatPromptTemplate.js";
+export * from "./OllamaCompletionModel.js";
 export { OllamaErrorData } from "./OllamaError.js";
 export * as ollama from "./OllamaFacade.js";
 export * from "./OllamaTextEmbeddingModel.js";
-export * from "./OllamaCompletionModel.js";
+export * from "./OllamaTextGenerationSettings.js";
