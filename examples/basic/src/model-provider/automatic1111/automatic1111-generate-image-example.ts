77dfe999029819669099ae01a80d5933d4d15c16
Lars Grammel <lars.grammel@gmail.com>
2023-12-21 22:50:48 +0100

    Update prompt.

--- examples/basic/src/model-provider/automatic1111/automatic1111-generate-image-example.ts
+++ examples/basic/src/model-provider/automatic1111/automatic1111-generate-image-example.ts
@@ -4,13 +4,15 @@ import fs from "node:fs";
 async function main() {
   const image = await generateImage(
     automatic1111.ImageGenerator({
-      model: "aZovyaRPGArtistTools_v3.safetensors [25ba966c5d]",
+      model: "aZovyaRPGArtistTools_v4.safetensors [4b99e70c19]",
       steps: 30,
       sampler: "DPM++ 2M Karras",
+      width: 512,
+      height: 512,
     }),
     {
       prompt:
-        "(lonely inn at the edge of a forest and at the side of a lake) (late spring evening before sunset) (clear sky) (plains) (nordic climate) (german early medieval architecture) (rpg) (fantasy) (Best quality) (masterpiece) (highly intricate details) (ultra realistic)",
+        "zprgstyle, lonely inn at the edge of a forest and at the side of a lake, late spring evening before sunset, clear sky, plains, nordic climate, german early medieval architecture, fantasy, Best quality, masterpiece, highly intricate details, ultra realistic",
       negativePrompt:
         "(worst quality, low quality, bad_pictures) (deformed, distorted, disfigured:1.3), poorly drawn, bad anatomy, wrong anatomy, extra limb, missing limb, floating limbs, (mutated hands and fingers:1.4), disconnected limbs, mutation, mutated, ugly, blurry, amputation, (watermark) (label) (signature) (text) (cropped) (castle) (mountain) (monastery)",
     }
