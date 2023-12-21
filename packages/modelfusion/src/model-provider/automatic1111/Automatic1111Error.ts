196160cecf8126133c5fd3108e0d698645a0241a
Lars Grammel <lars.grammel@gmail.com>
2023-12-21 23:21:07 +0100

    Simplify automatic1111 call implementation.

--- packages/modelfusion/src/model-provider/automatic1111/Automatic1111Error.ts
+++ packages/modelfusion/src/model-provider/automatic1111/Automatic1111Error.ts
@@ -1,10 +1,12 @@
 import { z } from "zod";
 import { ApiCallError } from "../../core/api/ApiCallError.js";
-import { ResponseHandler } from "../../core/api/postToApi.js";
+import {
+  ResponseHandler,
+  createJsonErrorResponseHandler,
+} from "../../core/api/postToApi.js";
 import { ZodSchema } from "../../core/schema/ZodSchema.js";
-import { parseJSON } from "../../core/schema/parseJSON.js";
 
-export const automatic1111ErrorDataSchema = new ZodSchema(
+const automatic1111ErrorDataSchema = new ZodSchema(
   z.object({
     error: z.string(),
     detail: z.string(),
@@ -16,42 +18,8 @@ export const automatic1111ErrorDataSchema = new ZodSchema(
 export type Automatic1111ErrorData =
   (typeof automatic1111ErrorDataSchema)["_type"];
 
-export class Automatic1111Error extends ApiCallError {
-  public readonly data: Automatic1111ErrorData;
-
-  constructor({
-    data,
-    statusCode,
-    url,
-    requestBodyValues,
-    message = data.detail,
-  }: {
-    message?: string;
-    statusCode: number;
-    url: string;
-    requestBodyValues: unknown;
-    data: Automatic1111ErrorData;
-  }) {
-    super({ message, statusCode, requestBodyValues, url });
-
-    this.data = data;
-  }
-}
-
-export const failedAutomatic1111CallResponseHandler: ResponseHandler<
-  ApiCallError
-> = async ({ response, url, requestBodyValues }) => {
-  const responseBody = await response.text();
-
-  const parsedError = parseJSON({
-    text: responseBody,
-    schema: automatic1111ErrorDataSchema,
+export const failedAutomatic1111CallResponseHandler: ResponseHandler<ApiCallError> =
+  createJsonErrorResponseHandler({
+    errorSchema: automatic1111ErrorDataSchema,
+    errorToMessage: (error) => error.detail,
   });
-
-  return new Automatic1111Error({
-    url,
-    requestBodyValues,
-    statusCode: response.status,
-    data: parsedError,
-  });
-};
--- packages/modelfusion/src/model-provider/automatic1111/Automatic1111ImageGenerationModel.ts
+++ packages/modelfusion/src/model-provider/automatic1111/Automatic1111ImageGenerationModel.ts
@@ -20,6 +20,19 @@ import {
   mapBasicPromptToAutomatic1111Format,
 } from "./Automatic1111ImageGenerationPrompt.js";
 
+export interface Automatic1111ImageGenerationSettings
+  extends ImageGenerationModelSettings {
+  api?: ApiConfiguration;
+
+  model: string;
+
+  height?: number;
+  width?: number;
+  sampler?: string;
+  steps?: number;
+  seed?: number;
+}
+
 /**
  * Create an image generation model that calls the AUTOMATIC1111 Stable Diffusion Web UI API.
  *
@@ -47,16 +60,35 @@ export class Automatic1111ImageGenerationModel
     input: Automatic1111ImageGenerationPrompt,
     options?: FunctionOptions
   ): Promise<Automatic1111ImageGenerationResponse> {
+    const api = this.settings.api ?? new Automatic1111ApiConfiguration();
+    const abortSignal = options?.run?.abortSignal;
+
     return callWithRetryAndThrottle({
       retry: this.settings.api?.retry,
       throttle: this.settings.api?.throttle,
-      call: async () =>
-        callAutomatic1111ImageGenerationAPI({
-          ...this.settings,
-
-          abortSignal: options?.run?.abortSignal,
-          prompt: input.prompt,
-        }),
+      call: async () => {
+        return postJsonToApi({
+          url: api.assembleUrl(`/txt2img`),
+          headers: api.headers,
+          body: {
+            height: this.settings.height,
+            width: this.settings.width,
+            prompt: input.prompt,
+            negative_prompt: input.negativePrompt,
+            sampler_index: this.settings.sampler,
+            steps: this.settings.steps,
+            seed: this.settings.seed,
+            override_settings: {
+              sd_model_checkpoint: this.settings.model,
+            },
+          },
+          failedResponseHandler: failedAutomatic1111CallResponseHandler,
+          successfulResponseHandler: createJsonResponseHandler(
+            Automatic1111ImageGenerationResponseSchema
+          ),
+          abortSignal,
+        });
+      },
     });
   }
 
@@ -109,18 +141,6 @@ export class Automatic1111ImageGenerationModel
   }
 }
 
-export interface Automatic1111ImageGenerationSettings
-  extends ImageGenerationModelSettings {
-  api?: ApiConfiguration;
-
-  model: string;
-
-  height?: number;
-  width?: number;
-  sampler?: string;
-  steps?: number;
-}
-
 const Automatic1111ImageGenerationResponseSchema = z.object({
   images: z.array(z.string()),
   parameters: z.object({}),
@@ -130,49 +150,3 @@ const Automatic1111ImageGenerationResponseSchema = z.object({
 export type Automatic1111ImageGenerationResponse = z.infer<
   typeof Automatic1111ImageGenerationResponseSchema
 >;
-
-async function callAutomatic1111ImageGenerationAPI({
-  api = new Automatic1111ApiConfiguration(),
-  abortSignal,
-  height,
-  width,
-  prompt,
-  negativePrompt,
-  sampler,
-  steps,
-  seed,
-  model,
-}: {
-  api?: ApiConfiguration;
-  abortSignal?: AbortSignal;
-  height?: number;
-  width?: number;
-  prompt: string;
-  negativePrompt?: string;
-  sampler?: string;
-  steps?: number;
-  seed?: number;
-  model?: string;
-}): Promise<Automatic1111ImageGenerationResponse> {
-  return postJsonToApi({
-    url: api.assembleUrl(`/txt2img`),
-    headers: api.headers,
-    body: {
-      height,
-      width,
-      prompt,
-      negative_prompt: negativePrompt,
-      sampler_index: sampler,
-      steps,
-      seed,
-      override_settings: {
-        sd_model_checkpoint: model,
-      },
-    },
-    failedResponseHandler: failedAutomatic1111CallResponseHandler,
-    successfulResponseHandler: createJsonResponseHandler(
-      Automatic1111ImageGenerationResponseSchema
-    ),
-    abortSignal,
-  });
-}
