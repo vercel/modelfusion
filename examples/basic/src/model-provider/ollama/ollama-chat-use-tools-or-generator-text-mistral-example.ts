d7f225af98052fdf15b9451eefbe35497266aa93
Lars Grammel <lars.grammel@gmail.com>
2023-12-23 17:29:34 +0100

    Add image generation tool call example.

---
+++ examples/basic/src/model-provider/ollama/ollama-chat-use-tools-or-generator-text-mistral-example.ts
@@ -0,0 +1,51 @@
+import dotenv from "dotenv";
+import {
+  automatic1111,
+  modelfusion,
+  ollama,
+  useToolsOrGenerateText,
+} from "modelfusion";
+import { openHermesToolCallsPromptTemplate } from "../../tool/prompts/open-hermes";
+import { ImageGeneratorTool } from "../../tool/tools/image-generator-tool";
+
+dotenv.config();
+
+modelfusion.setLogFormat("detailed-object");
+
+async function main() {
+  const { text, toolResults } = await useToolsOrGenerateText(
+    ollama
+      .ChatTextGenerator({
+        model: "mixtral",
+        temperature: 0,
+      })
+      .withInstructionPrompt()
+      .asToolCallsOrTextGenerationModel(openHermesToolCallsPromptTemplate),
+
+    [
+      new ImageGeneratorTool({
+        model: automatic1111
+          .ImageGenerator({
+            model: "rpg_v5",
+          })
+          .withTextPrompt(),
+      }),
+    ],
+
+    "Create a picture of a knight fighting a dragon"
+  );
+
+  if (text != null) {
+    console.log(`TEXT: ${text}`);
+  }
+
+  for (const { tool, toolCall, args, ok, result } of toolResults ?? []) {
+    console.log(`Tool call:`, toolCall);
+    console.log(`Tool:`, tool);
+    console.log(`Arguments:`, args);
+    console.log(`Ok:`, ok);
+    console.log(`Result or Error:`, result);
+  }
+}
+
+main().catch(console.error);
