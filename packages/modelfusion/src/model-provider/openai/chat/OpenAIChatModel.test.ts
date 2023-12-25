4a4ab65be018c7b051009f6a03f02edbcdccebc7
Lars Grammel <lars.grammel@gmail.com>
2023-12-25 17:04:27 +0100

    Add streamStructure test for OpenAI function calls.

--- packages/modelfusion/src/model-provider/openai/chat/OpenAIChatModel.test.ts
+++ packages/modelfusion/src/model-provider/openai/chat/OpenAIChatModel.test.ts
@@ -1,3 +1,6 @@
+import { z } from "zod";
+import { zodSchema } from "../../../core/schema/ZodSchema.js";
+import { streamStructure } from "../../../model-function/generate-structure/streamStructure.js";
 import { streamText } from "../../../model-function/generate-text/streamText.js";
 import { StreamingTestServer } from "../../../test/StreamingTestServer.js";
 import { arrayFromAsync } from "../../../test/arrayFromAsync.js";
@@ -38,5 +41,75 @@ describe("streamText", () => {
     );
 
     expect(await arrayFromAsync(stream)).toStrictEqual(["A"]);
+  });
+});
+
+describe("streamStructure", () => {
+  const server = new StreamingTestServer(
+    "https://api.openai.com/v1/chat/completions"
+  );
+
+  server.setupTestEnvironment();
+
+  it("should return a text stream", async () => {
+    server.responseChunks = [
+      `data: {"id":"chatcmpl-8ZhZtizjTCGmZaPRwyUiuDJ1DYUD0","object":"chat.completion.chunk",` +
+        `"created":1703519685,"model":"gpt-3.5-turbo-0613","system_fingerprint":null,` +
+        `"choices":[{"index":0,"delta":{"role":"assistant","content":null,` +
+        `"function_call":{"name":"generateCharacter","arguments":""}},"logprobs":null,"finish_reason":null}]}\n\n`,
+      `data: {"id":"chatcmpl-8ZhZtizjTCGmZaPRwyUiuDJ1DYUD0","object":"chat.completion.chunk",` +
+        `"created":1703519685,"model":"gpt-3.5-turbo-0613","system_fingerprint":null,` +
+        `"choices":[{"index":0,"delta":{"function_call":{"arguments":"{\\n"}},"logprobs":null,"finish_reason":null}]}\n\n`,
+      `data: {"id":"chatcmpl-8ZhZtizjTCGmZaPRwyUiuDJ1DYUD0","object":"chat.completion.chunk",` +
+        `"created":1703519685,"model":"gpt-3.5-turbo-0613","system_fingerprint":null,` +
+        `"choices":[{"index":0,"delta":{"function_call":{"arguments":" "}},"logprobs":null,"finish_reason":null}]}\n\n`,
+      `data: {"id":"chatcmpl-8ZhZtizjTCGmZaPRwyUiuDJ1DYUD0","object":"chat.completion.chunk",` +
+        `"created":1703519685,"model":"gpt-3.5-turbo-0613","system_fingerprint":null,` +
+        `"choices":[{"index":0,"delta":{"function_call":{"arguments":" \\""}},"logprobs":null,"finish_reason":null}]}\n\n`,
+      `data: {"id":"chatcmpl-8ZhZtizjTCGmZaPRwyUiuDJ1DYUD0","object":"chat.completion.chunk",` +
+        `"created":1703519685,"model":"gpt-3.5-turbo-0613","system_fingerprint":null,` +
+        `"choices":[{"index":0,"delta":{"function_call":{"arguments":"name"}},"logprobs":null,"finish_reason":null}]}\n\n`,
+      `data: {"id":"chatcmpl-8ZhZtizjTCGmZaPRwyUiuDJ1DYUD0","object":"chat.completion.chunk",` +
+        `"created":1703519685,"model":"gpt-3.5-turbo-0613","system_fingerprint":null,` +
+        `"choices":[{"index":0,"delta":{"function_call":{"arguments":"\\":\\""}},"logprobs":null,"finish_reason":null}]}\n\n`,
+      `data: {"id":"chatcmpl-8ZhZtizjTCGmZaPRwyUiuDJ1DYUD0","object":"chat.completion.chunk",` +
+        `"created":1703519685,"model":"gpt-3.5-turbo-0613","system_fingerprint":null,` +
+        `"choices":[{"index":0,"delta":{"function_call":{"arguments":"M"}},"logprobs":null,"finish_reason":null}]}\n\n`,
+      `data: {"id":"chatcmpl-8ZhZtizjTCGmZaPRwyUiuDJ1DYUD0","object":"chat.completion.chunk",` +
+        `"created":1703519685,"model":"gpt-3.5-turbo-0613","system_fingerprint":null,` +
+        `"choices":[{"index":0,"delta":{"function_call":{"arguments":"ike\\"\\n"}},"logprobs":null,"finish_reason":null}]}\n\n`,
+      `data: {"id":"chatcmpl-8ZhZtizjTCGmZaPRwyUiuDJ1DYUD0","object":"chat.completion.chunk",` +
+        `"created":1703519685,"model":"gpt-3.5-turbo-0613","system_fingerprint":null,` +
+        `"choices":[{"index":0,"delta":{"function_call":{"arguments":"}"}},"logprobs":null,"finish_reason":null}]}\n\n`,
+      `data: {"id":"chatcmpl-8ZhZtizjTCGmZaPRwyUiuDJ1DYUD0","object":"chat.completion.chunk",` +
+        `"created":1703519685,"model":"gpt-3.5-turbo-0613","system_fingerprint":null,` +
+        `"choices":[{"index":0,"delta":{},"logprobs":null,"finish_reason":"stop"}]}\n\n`,
+      `data: [DONE]\n\n`,
+    ];
+
+    const stream = await streamStructure(
+      new OpenAIChatModel({
+        api: new OpenAIApiConfiguration({ apiKey: "test-key" }),
+        model: "gpt-3.5-turbo",
+      })
+        .asFunctionCallStructureGenerationModel({
+          fnName: "generateCharacter",
+          fnDescription: "Generate character descriptions.",
+        })
+        .withTextPrompt(),
+
+      zodSchema(z.object({ name: z.string() })),
+
+      "generate a name"
+    );
+
+    // note: space moved to last chunk bc of trimming
+    expect(await arrayFromAsync(stream)).toStrictEqual([
+      { isComplete: false, value: {} },
+      { isComplete: false, value: { name: "" } },
+      { isComplete: false, value: { name: "M" } },
+      { isComplete: false, value: { name: "Mike" } },
+      { isComplete: true, value: { name: "Mike" } },
+    ]);
   });
 });
