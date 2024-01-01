8097c2a7256b0559e832316afdb98dcd33caa47d
Lars Grammel <lars.grammel@gmail.com>
2024-01-01 13:08:16 +0100

    Fix exports.

--- packages/modelfusion/src/model-provider/llamacpp/index.ts
+++ packages/modelfusion/src/model-provider/llamacpp/index.ts
@@ -1,6 +1,6 @@
 export * from "./LlamaCppApiConfiguration.js";
 export * as LlamaCppBakLLaVA1Prompt from "./LlamaCppBakLLaVA1PromptTemplate.js";
-export { LlamaCppError, LlamaCppErrorData } from "./LlamaCppError.js";
+export { LlamaCppErrorData } from "./LlamaCppError.js";
 export * as llamacpp from "./LlamaCppFacade.js";
 export * from "./LlamaCppTextEmbeddingModel.js";
 export * from "./LlamaCppCompletionModel.js";
