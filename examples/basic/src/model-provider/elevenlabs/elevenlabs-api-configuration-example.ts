8751493fd98dba7803836a9fc483f00591115363
Lars Grammel <lars.grammel@gmail.com>
2024-01-01 10:38:38 +0100

    Add elevenlabs.Api

---
+++ examples/basic/src/model-provider/elevenlabs/elevenlabs-api-configuration-example.ts
@@ -0,0 +1,25 @@
+import dotenv from "dotenv";
+import { elevenlabs, generateSpeech } from "modelfusion";
+import fs from "node:fs";
+
+dotenv.config();
+
+async function main() {
+  const speech = await generateSpeech(
+    elevenlabs.SpeechGenerator({
+      api: elevenlabs.Api({
+        apiKey: process.env.ELEVENLABS_API_KEY,
+      }),
+      voice: "pNInz6obpgDQGcFmaJgB", // Adam
+    }),
+    "Good evening, ladies and gentlemen! Exciting news on the airwaves tonight " +
+      "as The Rolling Stones unveil 'Hackney Diamonds,' their first collection of " +
+      "fresh tunes in nearly twenty years, featuring the illustrious Lady Gaga, the " +
+      "magical Stevie Wonder, and the final beats from the late Charlie Watts."
+  );
+
+  const path = `./elevenlabs-speech-example.mp3`;
+  fs.writeFileSync(path, speech);
+}
+
+main().catch(console.error);
--- packages/modelfusion/src/model-provider/elevenlabs/ElevenLabsFacade.ts
+++ packages/modelfusion/src/model-provider/elevenlabs/ElevenLabsFacade.ts
@@ -1,7 +1,21 @@
+import { PartialBaseUrlPartsApiConfigurationOptions } from "../../core/api/BaseUrlApiConfiguration.js";
+import { ElevenLabsApiConfiguration } from "./ElevenLabsApiConfiguration.js";
 import {
   ElevenLabsSpeechModel,
   ElevenLabsSpeechModelSettings,
 } from "./ElevenLabsSpeechModel.js";
+
+/**
+ * Creates an API configuration for the ElevenLabs API.
+ * It calls the API at https://api.elevenlabs.io/v1 and uses the `ELEVENLABS_API_KEY` env variable by default.
+ */
+export function Api(
+  settings: PartialBaseUrlPartsApiConfigurationOptions & {
+    apiKey?: string;
+  }
+) {
+  return new ElevenLabsApiConfiguration(settings);
+}
 
 /**
  * Synthesize speech using the ElevenLabs Text to Speech API.
