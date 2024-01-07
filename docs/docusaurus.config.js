95ee96b9cb0cfe00241aa626325d20427653c9d3
Lars Grammel <lars.grammel@gmail.com>
2024-01-07 19:05:42 +0100

    Attempt to fix social card.

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -116,7 +116,7 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
-      image: "img/social-card.jpg",
+      image: "/img/social-card.png",
       navbar: {
         title: "ModelFusion",
         items: [
