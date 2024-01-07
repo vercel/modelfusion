b8e72320326fe930c1d306a18446be52dfb3d9a4
Lars Grammel <lars.grammel@gmail.com>
2024-01-07 19:22:22 +0100

    Trigger rebuild.

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -26,9 +26,6 @@ const config = {
     },
   ],
 
-  // Even if you don't use internalization, you can use this field to set useful
-  // metadata like html lang. For example, if your site is Chinese, you may want
-  // to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: "en",
     locales: ["en"],
