9d18d30ac78e08f8b2797e5b26850dcfbd44914c
Lars Grammel <lars.grammel@gmail.com>
2024-01-09 20:00:48 +0100

    Add mistral prompt docs.

--- packages/modelfusion/src/model-provider/ollama/OllamaCompletionPrompt.ts
+++ packages/modelfusion/src/model-provider/ollama/OllamaCompletionPrompt.ts
@@ -34,8 +34,42 @@ export function asOllamaCompletionTextPromptTemplateProvider(
 }
 
 export const Text = asOllamaCompletionTextPromptTemplateProvider(textPrompt);
+
+/**
+ * Formats text, instruction or chat prompts as a Mistral instruct prompt.
+ *
+ * Note that Mistral does not support system prompts. We emulate them.
+ *
+ * Text prompt:
+ * ```
+ * <s>[INST] { instruction } [/INST]
+ * ```
+ *
+ * Instruction prompt when system prompt is set:
+ * ```
+ * <s>[INST] ${ system prompt } [/INST] </s>[INST] ${instruction} [/INST] ${ response prefix }
+ * ```
+ *
+ * Instruction prompt template when there is no system prompt:
+ * ```
+ * <s>[INST] ${ instruction } [/INST] ${ response prefix }
+ * ```
+ *
+ * Chat prompt when system prompt is set:
+ * ```
+ * <s>[INST] ${ system prompt } [/INST] </s> [INST] ${ user msg 1 } [/INST] ${ model response 1 } [INST] ${ user msg 2 } [/INST] ${ model response 2 } [INST] ${ user msg 3 } [/INST]
+ * ```
+ *
+ * Chat prompt when there is no system prompt:
+ * ```
+ * <s>[INST] ${ user msg 1 } [/INST] ${ model response 1 } </s>[INST] ${ user msg 2 } [/INST] ${ model response 2 } [INST] ${ user msg 3 } [/INST]
+ * ```
+ *
+ * @see https://docs.mistral.ai/models/#chat-template
+ */
 export const Mistral =
   asOllamaCompletionTextPromptTemplateProvider(mistralPrompt);
+
 export const ChatML =
   asOllamaCompletionTextPromptTemplateProvider(chatMlPrompt);
 export const Llama2 =
