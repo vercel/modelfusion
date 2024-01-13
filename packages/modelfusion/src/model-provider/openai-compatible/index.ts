c5c2e68d18f17e1692ebc10ca3b6797b627f9e09
Lars Grammel <lars.grammel@gmail.com>
2024-01-13 21:03:32 +0100

    Add OpenAICompatibleTextEmbeddingModel.

--- packages/modelfusion/src/model-provider/openai-compatible/index.ts
+++ packages/modelfusion/src/model-provider/openai-compatible/index.ts
@@ -1,6 +1,7 @@
 export * from "./FireworksAIApiConfiguration.js";
 export * from "./OpenAICompatibleChatModel.js";
 export * from "./OpenAICompatibleCompletionModel.js";
+export * from "./OpenAICompatibleTextEmbeddingModel.js";
 export * as openaicompatible from "./OpenAICompatibleFacade.js";
 export * from "./OpenAICompatibleProviderName.js";
 export * from "./TogetherAIApiConfiguration.js";
