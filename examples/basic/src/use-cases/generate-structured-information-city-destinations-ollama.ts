a4c530cfc11b78dff72b51c7e798cf607a45a097
Lars Grammel <lars.grammel@gmail.com>
2024-01-18 12:33:04 +0100

    Add Ollama structure generation example.

--- examples/basic/src/use-cases/generate-structured-information-city-destinations-ollama.ts
+++ examples/basic/src/use-cases/generate-structured-information-city-destinations-ollama.ts
@@ -1,33 +1,30 @@
 import {
   generateStructure,
   jsonStructurePrompt,
-  llamacpp,
+  ollama,
   zodSchema,
 } from "modelfusion";
 import { z } from "zod";
 
-/**
- * Ask an LLM to output well-represented information (that is unlikely to be hallucinated)
- * in a structured format.
- */
 const listCityDestinations = (country: string) =>
   generateStructure({
-    model: llamacpp
-      .CompletionTextGenerator({
-        // run Mistral or Mixtral instruct model with llama.cpp
-        promptTemplate: llamacpp.prompt.Mistral,
+    model: ollama
+      .ChatTextGenerator({
+        model: "nous-hermes2-mixtral",
         temperature: 0,
       })
       .asStructureGenerationModel(jsonStructurePrompt.text()),
 
     schema: zodSchema(
-      z.array(
-        z.object({
-          city: z.string(),
-          region: z.string(),
-          description: z.string(),
-        })
-      )
+      z.object({
+        destinations: z.array(
+          z.object({
+            city: z.string(),
+            region: z.string(),
+            description: z.string(),
+          })
+        ),
+      })
     ),
 
     prompt:
