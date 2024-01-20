76598f8d4a6f9a91c1d39f8dd458f16229d0256c
Lars Grammel <lars.grammel@gmail.com>
2024-01-20 10:43:12 +0100

    Add multi-modal example.

---
+++ examples/basic/src/model-function/stream-text-multimodal-example.ts
@@ -0,0 +1,32 @@
+import dotenv from "dotenv";
+import { openai, streamText } from "modelfusion";
+import fs from "node:fs";
+import path from "node:path";
+
+dotenv.config();
+
+async function main() {
+  const image = fs.readFileSync(path.join("data", "example-image.png"), {
+    encoding: "base64",
+  });
+
+  const textStream = await streamText({
+    model: openai.ChatTextGenerator({
+      model: "gpt-4-vision-preview",
+      maxGenerationTokens: 1000,
+    }),
+
+    prompt: [
+      openai.ChatMessage.user([
+        { type: "text", text: "Describe the image in detail:\n\n" },
+        { type: "image", image, mimeType: "image/png" },
+      ]),
+    ],
+  });
+
+  for await (const textPart of textStream) {
+    process.stdout.write(textPart);
+  }
+}
+
+main().catch(console.error);
