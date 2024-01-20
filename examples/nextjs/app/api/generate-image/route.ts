5f495df690a447ee81927c86b0a7720c0f84c704
Lars Grammel <lars.grammel@gmail.com>
2024-01-20 13:25:36 +0100

    Add error handling.

--- examples/nextjs/app/api/generate-image/route.ts
+++ examples/nextjs/app/api/generate-image/route.ts
@@ -5,32 +5,29 @@ export const runtime = "edge";
 export async function POST(req: Request) {
   const { text: description } = await req.json();
 
-  const { imageBase64 } = await generateImage({
-    model: stability.ImageGenerator({
-      // explicit API configuration needed for NextJS environment
-      // (otherwise env variables are not available):
-      api: stability.Api({ apiKey: process.env.STABILITY_API_KEY }),
-      model: "stable-diffusion-v1-6",
-      clipGuidancePreset: "FAST_BLUE",
-      cfgScale: 10,
-      height: 512,
-      width: 512,
-      steps: 30,
-    }),
-    prompt: [
-      { text: description },
-      { text: "style of early 19th century painting", weight: 0.5 },
-    ],
-    fullResponse: true,
-  });
-
   try {
+    const { imageBase64 } = await generateImage({
+      model: stability.ImageGenerator({
+        // explicit API configuration needed for NextJS environment
+        // (otherwise env variables are not available):
+        api: stability.Api({ apiKey: process.env.STABILITY_API_KEY }),
+        model: "stable-diffusion-v1-6",
+        clipGuidancePreset: "FAST_BLUE",
+        cfgScale: 10,
+        height: 512,
+        width: 512,
+        steps: 30,
+      }),
+      prompt: [
+        { text: description },
+        { text: "style of early 19th century painting", weight: 0.5 },
+      ],
+      fullResponse: true,
+    });
+
     return Response.json(imageBase64);
-  } catch (error) {
+  } catch (error: any) {
     console.error(error);
-    return Response.json(
-      { message: error?.toString(), error },
-      { status: 500 }
-    );
+    return new Response(error.message, { status: 500 });
   }
 }
--- examples/nextjs/app/generate-image/page.tsx
+++ examples/nextjs/app/generate-image/page.tsx
@@ -1,18 +1,23 @@
 "use client";
 
-import { Input } from "@/components/ui/input";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { Button } from "@/components/ui/button";
-import { useState } from "react";
+import { Input } from "@/components/ui/input";
 import { Skeleton } from "@/components/ui/skeleton";
+import { ExclamationTriangleIcon } from "@radix-ui/react-icons";
+import { useState } from "react";
 
 export default function () {
   const [inputValue, setInputValue] = useState("");
   const [imageSrc, setImageSrc] = useState<string | null>(null);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     setIsLoading(true);
+    setImageSrc(null);
+    setError(null);
 
     try {
       const response = await fetch("/api/generate-image", {
@@ -26,7 +31,11 @@ export default function () {
       if (response.ok) {
         const image = await response.json();
         setImageSrc(`data:image/png;base64,${image}`);
+        return;
       }
+
+      const errorText = await response.text();
+      setError(errorText);
     } finally {
       setIsLoading(false);
     }
@@ -57,6 +66,13 @@ export default function () {
           </Button>
         </form>
       </div>
+      {error && (
+        <Alert variant="destructive">
+          <ExclamationTriangleIcon className="h-4 w-4" />
+          <AlertTitle>Error</AlertTitle>
+          <AlertDescription>{error}</AlertDescription>
+        </Alert>
+      )}
       <div className="w-[512px] h-[512px] space-y-2">
         {isLoading ? (
           <Skeleton className="h-[512px] w-[512px]" />
