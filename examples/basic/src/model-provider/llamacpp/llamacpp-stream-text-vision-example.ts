199ed30bee8d51ba95cd1ec51e5cf72b90a57395
Lars Grammel <lars.grammel@gmail.com>
2024-01-20 11:08:57 +0100

    Fix example (raw prompt).

--- examples/basic/src/model-provider/llamacpp/llamacpp-stream-text-vision-example.ts
+++ examples/basic/src/model-provider/llamacpp/llamacpp-stream-text-vision-example.ts
@@ -3,7 +3,9 @@ import fs from "node:fs";
 import path from "node:path";
 
 async function main() {
-  const image = fs.readFileSync(path.join("data", "comic-mouse.png"));
+  const image = fs.readFileSync(path.join("data", "comic-mouse.png"), {
+    encoding: "base64",
+  });
 
   const textStream = await streamText({
     model: llamacpp.CompletionTextGenerator({
