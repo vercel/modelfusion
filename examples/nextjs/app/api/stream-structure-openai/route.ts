a5d250a175671bd17b59ec298c8ae78e6550b99d
Lars Grammel <lars.grammel@gmail.com>
2024-01-22 22:09:36 +0100

    Add OpenAI stream structure example with route.

---
+++ examples/nextjs/app/api/stream-structure-openai/route.ts
@@ -0,0 +1,49 @@
+import { jsonStructurePrompt, openai, streamStructure } from "modelfusion";
+import { itinerarySchema } from "../../stream-structure-openai/itinerarySchema";
+
+export const runtime = "edge";
+
+export async function POST(req: Request) {
+  const { destination, lengthOfStay } = await req.json();
+
+  const stream = await streamStructure({
+    model: openai
+      .ChatTextGenerator({
+        model: "gpt-4-1106-preview",
+        maxGenerationTokens: 2500,
+      })
+      .asStructureGenerationModel(jsonStructurePrompt.instruction()),
+
+    schema: itinerarySchema,
+
+    prompt: {
+      system:
+        "You help planning travel itineraries. " +
+        "Respond to the users' request with a list of the best stops to make in their destination.",
+
+      instruction: `I am planning a trip to ${destination} for ${lengthOfStay} days.`,
+    },
+  });
+
+  const textEncoder = new TextEncoder();
+
+  return new Response(
+    new ReadableStream({
+      async start(controller) {
+        try {
+          for await (const { textDelta } of stream) {
+            controller.enqueue(textEncoder.encode(textDelta));
+          }
+        } finally {
+          controller.close();
+        }
+      },
+    }),
+    {
+      status: 200,
+      headers: {
+        "Content-Type": "text/plain; charset=utf-8",
+      },
+    }
+  );
+}
---
+++ examples/nextjs/app/stream-structure-openai/useItinerary.tsx
@@ -0,0 +1,37 @@
+import { parseJsonStreamResponse } from "modelfusion";
+import { useState } from "react";
+import { Itinerary } from "./itinerarySchema";
+
+export function useItinerary() {
+  const [isGenerating, setIsGenerating] = useState(false);
+  const [itinerary, setItinerary] = useState<Itinerary>();
+
+  const generateItinerary = async ({
+    destination,
+    lengthOfStay,
+  }: {
+    destination: string;
+    lengthOfStay: string;
+  }) => {
+    setItinerary(undefined);
+    setIsGenerating(true);
+
+    try {
+      await parseJsonStreamResponse<Itinerary>({
+        response: await fetch("/api/stream-structure-openai", {
+          method: "POST",
+          body: JSON.stringify({ destination, lengthOfStay }),
+        }),
+        onChunk: setItinerary,
+      });
+    } finally {
+      setIsGenerating(false);
+    }
+  };
+
+  return {
+    isGeneratingItinerary: isGenerating,
+    generateItinerary,
+    itinerary,
+  };
+}
