12e7b7949b6d82e9926e675645feed6764647ace
Lars Grammel <lars.grammel@gmail.com>
2024-01-22 22:46:50 +0100

    Add useCallback.

--- examples/nextjs/app/stream-structure-openai/useItinerary.tsx
+++ examples/nextjs/app/stream-structure-openai/useItinerary.tsx
@@ -1,33 +1,36 @@
 import { parseStructureStreamResponse } from "modelfusion";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { Itinerary } from "./itinerarySchema";
 
 export function useItinerary() {
   const [isGenerating, setIsGenerating] = useState(false);
   const [itinerary, setItinerary] = useState<Itinerary>();
 
-  const generateItinerary = async ({
-    destination,
-    lengthOfStay,
-  }: {
-    destination: string;
-    lengthOfStay: string;
-  }) => {
-    setItinerary(undefined);
-    setIsGenerating(true);
+  const generateItinerary = useCallback(
+    async ({
+      destination,
+      lengthOfStay,
+    }: {
+      destination: string;
+      lengthOfStay: string;
+    }) => {
+      setItinerary(undefined);
+      setIsGenerating(true);
 
-    try {
-      await parseStructureStreamResponse<Itinerary>({
-        response: await fetch("/api/stream-structure-openai", {
-          method: "POST",
-          body: JSON.stringify({ destination, lengthOfStay }),
-        }),
-        onChunk: setItinerary,
-      });
-    } finally {
-      setIsGenerating(false);
-    }
-  };
+      try {
+        await parseStructureStreamResponse<Itinerary>({
+          response: await fetch("/api/stream-structure-openai", {
+            method: "POST",
+            body: JSON.stringify({ destination, lengthOfStay }),
+          }),
+          onChunk: setItinerary,
+        });
+      } finally {
+        setIsGenerating(false);
+      }
+    },
+    []
+  );
 
   return {
     isGeneratingItinerary: isGenerating,
