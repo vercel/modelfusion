ebf43df940f6dd977ee63666b56862f4f1c8fa8c
Lars Grammel <lars.grammel@gmail.com>
2024-01-24 20:27:55 +0100

    Show all blog posts in sidebar.

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -52,6 +52,10 @@ const config = {
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
         },
+        blog: {
+          blogSidebarTitle: "All posts",
+          blogSidebarCount: "ALL",
+        },
       }),
     ],
   ],
