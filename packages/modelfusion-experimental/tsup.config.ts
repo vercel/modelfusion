b063b9d0f8303fe37ca0bd0f08f894168043d56b
Jake Detels <jake.detels@gmail.com>
2024-02-16 15:31:43 -0800

    support TypeScript declaration maps for local development

--- packages/modelfusion-experimental/tsup.config.ts
+++ packages/modelfusion-experimental/tsup.config.ts
@@ -4,21 +4,18 @@ export default defineConfig([
   {
     entry: ["src/index.ts"],
     format: ["cjs", "esm"],
-    dts: true,
     sourcemap: true,
   },
   {
     entry: ["src/browser/index.ts"],
     format: ["cjs", "esm"],
     outDir: "dist/browser",
-    dts: true,
     sourcemap: true,
   },
   {
     entry: ["src/server/fastify/index.ts"],
     format: ["cjs", "esm"],
     outDir: "dist/fastify-server",
-    dts: true,
     sourcemap: true,
   },
 ]);
